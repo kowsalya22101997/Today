7a3787af5be0208aad207f037f634383a60646b9
kowsalyaguru <kowsalyaguru22@gmail.com>
2021-03-25 13:18:26 +0530

    good

---
+++ src/org/Bujji/Sharp.java
@@ -0,0 +1,35 @@
+package org.Bujji;
+
+public class Sharp {
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b=15;
+		int c;
+		c=a+b;
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
